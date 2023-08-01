778e071aa660ce0c765ac6fdbba9c39a15b60839
roysonali829 <roysonali829@gmail.com>
2023-08-01 12:01:14 +0530

    first commit

---
+++ app3/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def new3(request):
+    return render(request,'new3.html')
--- project11/urls.py
+++ project11/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path,include
 import app1,app2
 
+from app3.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/',include('app1.urls')),
     path('app2/',include('app2.urls')),
+    path('new3/',new3,name='new3'),
 ]
